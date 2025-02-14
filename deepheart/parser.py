d6a0a5efeed5abb4708c5d037ddec80a5b275055
MOHITRAJDEO13158920 <mohitrajdeo16deoghar@gmail.com>
2025-02-14 17:12:52 +0530

    Update parser.py

--- deepheart/parser.py
+++ deepheart/parser.py
@@ -289,261 +289,315 @@
 
 
 
-import os
-import pickle
-import numpy as np
-from scipy.io import wavfile
-from scipy.fftpack import fft
-from scipy.signal import butter, lfilter
-from sklearn.preprocessing import normalize
-from sklearn.model_selection import train_test_split
-from collections import namedtuple
-from sklearn.utils import check_random_state
+# import os
+# import pickle
+# import numpy as np
+# from scipy.io import wavfile
+# from scipy.fftpack import fft
+# from scipy.signal import butter, lfilter
+# from sklearn.preprocessing import normalize
+# from sklearn.model_selection import train_test_split
+# from collections import namedtuple
+# from sklearn.utils import check_random_state
 
 
-class PCG:
+# class PCG:
+#     """
+#     PCG is a container for loading phonocardiogram (PCG) data for the [2016 physionet
+#     challenge](http://physionet.org/challenge/2016). Raw wav files are parsed into
+#     features, class labels are extracted from header files, and data is split into
+#     training and testing groups.
+#     """
+#     def __init__(self, basepath, random_state=42):
+#         self.basepath = basepath
+#         self.class_name_to_id = {"normal": 0, "abnormal": 1}
+#         self.nclasses = len(self.class_name_to_id.keys())
+
+#         self.train = None
+#         self.test = None
+
+#         self.n_samples = 0
+
+#         self.X = None
+#         self.y = None
+
+#         self.random_state = random_state
+
+#     def initialize_wav_data(self):
+#         """
+#         Load the original wav files and extract features. Warning, this can take a
+#         while due to slow FFTs.
+
+#         Parameters
+#         ----------
+#         None
+
+#         Returns
+#         -------
+#         None
+#         """
+#         self.__load_wav_file()
+#         self.__split_train_test()
+#         # Save the processed data
+#         self.save("/tmp")
+
+#     def save(self, save_path):
+#         """
+#         Persist the PCG class to disk.
+
+#         Parameters
+#         ----------
+#         save_path: str
+#             Location on disk to store the parsed PCG metadata.
+
+#         Returns
+#         -------
+#         None
+#         """
+#         if not os.path.exists(save_path):
+#             os.makedirs(save_path)
+#         np.save(os.path.join(save_path, "X.npy"), self.X)
+#         np.save(os.path.join(save_path, "y.npy"), self.y)
+#         with open(os.path.join(save_path, "meta.pkl"), "wb") as fout:
+#             pickle.dump((self.basepath, self.class_name_to_id, self.nclasses,
+#                          self.n_samples, self.random_state), fout)
+
+#     def load(self, load_path):
+#         """
+#         Load a previously stored PCG class.
+
+#         Parameters
+#         ----------
+#         load_path: str
+#             Location on disk to load parsed PCG data.
+
+#         Returns
+#         -------
+#         None
+#         """
+#         self.X = np.load(os.path.join(load_path, "X.npy"))
+#         self.y = np.load(os.path.join(load_path, "y.npy"))
+#         with open(os.path.join(load_path, "meta.pkl"), "rb") as fin:
+#             (self.basepath, self.class_name_to_id, self.nclasses,
+#              self.n_samples, self.random_state) = pickle.load(fin)
+#         self.__split_train_test()
+
+#     def __load_wav_file(self, doFFT=True):
+#         """
+#         Loads physio 2016 challenge dataset from self.basepath by crawling the path.
+#         For each discovered wav file:
+
+#         * Attempt to parse the header file for class label.
+#         * Attempt to load the wav file.
+#         * Calculate features from the wav file. If doFFT, features are
+#           the Fourier transform of the original signal. Else, features are
+#           the raw signal itself truncated to a fixed length.
+
+#         Parameters
+#         ----------
+#         doFFT: bool
+#             True if features to be calculated are the FFT of the original signal.
+
+#         Returns
+#         -------
+#         None
+#         """
+#         # First pass to calculate number of samples
+#         wav_file_names = []
+#         class_labels = []
+#         for root, dirs, files in os.walk(self.basepath):
+#             # Ignore validation for now!
+#             if "validation" in root:
+#                 continue
+#             for file in files:
+#                 if file.endswith('.wav'):
+#                     try:
+#                         base_file_name = file.rstrip(".wav")
+#                         label_file_name = os.path.join(root, base_file_name + ".hea")
+
+#                         class_label = self.__parse_class_label(label_file_name)
+#                         class_labels.append(self.class_name_to_id[class_label])
+#                         wav_file_names.append(os.path.join(root, file))
+
+#                         self.n_samples += 1
+#                     except InvalidHeaderFileException as e:
+#                         print(e)
+
+#         if doFFT:
+#             fft_embedding_size = 400
+#             highpass_embedding_size = 200
+#             X = np.zeros([self.n_samples, fft_embedding_size + highpass_embedding_size])
+#         else:
+#             # Truncating the length of each wav file to the
+#             # min file size (10611) (Note: this is bad
+#             # And causes loss of information!)
+#             embedding_size = 10611
+#             X = np.zeros([self.n_samples, embedding_size])
+
+#         for idx, wavfname in enumerate(wav_file_names):
+#             rate, wf = wavfile.read(wavfname)
+#             wf = normalize(wf.reshape(1, -1), axis=1)  # Normalize along the time axis
+
+#             if doFFT:
+#                 # We only care about the magnitude of each frequency
+#                 wf_fft = np.abs(fft(wf))
+#                 wf_fft = wf_fft[:, :fft_embedding_size].reshape(-1)
+
+#                 # Filter out high frequencies via Butterworth filter
+#                 nyquist = 0.5 * rate
+#                 cutoff_freq = 4.0  # Hz
+#                 b, a = butter(5, cutoff_freq / nyquist, btype='low', analog=False)
+#                 wf_low_pass = lfilter(b, a, wf)
+
+#                 # FFT the filtered signal
+#                 wf_low_pass_fft = np.abs(fft(wf_low_pass))
+#                 wf_low_pass_fft = wf_low_pass_fft[:, :highpass_embedding_size].reshape(-1)
+
+#                 features = np.concatenate((wf_fft, wf_low_pass_fft))
+#             else:
+#                 features = wf[:, :embedding_size].reshape(-1)
+
+#             X[idx, :] = features
+
+#         self.X = X
+#         self.y = np.eye(self.nclasses)[class_labels]
+
+#     def __parse_class_label(self, label_file_name):
+#         """
+#         Parses physio bank header files, where the class label
+#         is located in the last line of the file.
+
+#         Parameters
+#         ----------
+#         label_file_name: str
+#             Path to a specific header file.
+
+#         Returns
+#         -------
+#         class_label: str
+#             One of `normal` or `abnormal`.
+#         """
+#         with open(label_file_name, 'r') as fin:
+#             header = fin.readlines()
+
+#         comments = [line for line in header if line.startswith("#")]
+#         if not len(comments) == 1:
+#             raise InvalidHeaderFileException("Invalid label file %s" % label_file_name)
+
+#         class_label = str(comments[0]).lstrip("#").rstrip("\r").strip().lower()
+
+#         if class_label not in self.class_name_to_id.keys():
+#             raise InvalidHeaderFileException("Invalid class label %s" % class_label)
+
+#         return class_label
+
+#     def __split_train_test(self):
+#         """
+#         Splits internal features (self.X) and class labels (self.y) into
+#         balanced training and test sets using sklearn's helper function.
+
+#         Returns
+#         -------
+#         None
+#         """
+#         mlData = namedtuple('ml_data', 'X y')
+
+#         num_pos, num_neg = np.sum(self.y, axis=0)
+
+#         # Remove samples to rebalance classes
+#         undersample_rate = num_neg / num_pos
+#         over_represented_idxs = self.y[:, 1] == 0
+#         under_represented_idxs = self.y[:, 1] == 1
+#         random_indexes_to_remove = np.random.rand(self.y.shape[0]) < undersample_rate
+#         sample_idxs = (over_represented_idxs & random_indexes_to_remove) | under_represented_idxs
+
+#         X_balanced = self.X[sample_idxs, :]
+#         y_balanced = self.y[sample_idxs, :]
+
+#         X_train, X_test, y_train, y_test = train_test_split(X_balanced, y_balanced, test_size=0.25,
+#                                                             random_state=self.random_state)
+
+#         self.train = mlData(X=X_train, y=y_train)
+#         self.test = mlData(X=X_test, y=y_test)
+
+#     def get_mini_batch(self, batch_size):
+#         """
+#         Helper function for sampling mini-batches from the training set.
+
+#         Parameters
+#         ----------
+#         batch_size: int
+#             Number of elements to return in the mini batch.
+
+#         Returns
+#         -------
+#         X: np.ndarray
+#             A feature matrix subsampled from self.train.
+
+#         y: np.ndarray
+#             A one-hot matrix of class labels subsampled from self.train.
+#         """
+#         random_state = check_random_state(None)  # self.random_state)
+#         n_training_samples = self.train.X.shape[0]
+#         minibatch_indices = random_state.randint(0, n_training_samples, batch_size)
+
+#         return self.train.X[minibatch_indices, :], self.train.y[minibatch_indices, :]
+
+
+# class InvalidHeaderFileException(Exception):
+#     def __init__(self, message):
+#         super().__init__(message)
+
+
+from parser import PCG
+from model import CNN
+import sys
+
+# Define valid strings for "True"
+true_strs = {"True", "true", "t", "1", "yes"}
+
+def load_and_train_model(model_path, load_pretrained):
     """
-    PCG is a container for loading phonocardiogram (PCG) data for the [2016 physionet
-    challenge](http://physionet.org/challenge/2016). Raw wav files are parsed into
-    features, class labels are extracted from header files, and data is split into
-    training and testing groups.
+    Load the dataset and train the CNN model.
+
+    Parameters
+    ----------
+    model_path : str
+        Path to the dataset directory.
+    load_pretrained : bool
+        Whether to load preprocessed data from disk.
     """
-    def __init__(self, basepath, random_state=42):
-        self.basepath = basepath
-        self.class_name_to_id = {"normal": 0, "abnormal": 1}
-        self.nclasses = len(self.class_name_to_id.keys())
+    # Initialize the PCG dataset
+    pcg = PCG(model_path)
 
-        self.train = None
-        self.test = None
+    if load_pretrained:
+        # Load preprocessed data
+        pcg.load("/tmp")
+    else:
+        # Process raw data and save it
+        pcg.initialize_wav_data()
 
-        self.n_samples = 0
+    # Initialize and train the CNN model
+    cnn = CNN(pcg, epochs=100, dropout=0.5)
+    cnn.train()
 
-        self.X = None
-        self.y = None
+if __name__ == '__main__':
+    # Check for correct number of arguments
+    if len(sys.argv) < 2:
+        print("Usage: python train.py <data_path> [load_pretrained]")
+        print("Example: python train.py ./data true")
+        sys.exit(1)
 
-        self.random_state = random_state
+    # Parse command-line arguments
+    data_path = sys.argv[1]
 
-    def initialize_wav_data(self):
-        """
-        Load the original wav files and extract features. Warning, this can take a
-        while due to slow FFTs.
+    # Default to False if no second argument is provided
+    load_pretrained = False
+    if len(sys.argv) == 3:
+        load_pretrained = sys.argv[2].lower() in true_strs
 
-        Parameters
-        ----------
-        None
-
-        Returns
-        -------
-        None
-        """
-        self.__load_wav_file()
-        self.__split_train_test()
-        # Save the processed data
-        self.save("/tmp")
-
-    def save(self, save_path):
-        """
-        Persist the PCG class to disk.
-
-        Parameters
-        ----------
-        save_path: str
-            Location on disk to store the parsed PCG metadata.
-
-        Returns
-        -------
-        None
-        """
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-        np.save(os.path.join(save_path, "X.npy"), self.X)
-        np.save(os.path.join(save_path, "y.npy"), self.y)
-        with open(os.path.join(save_path, "meta.pkl"), "wb") as fout:
-            pickle.dump((self.basepath, self.class_name_to_id, self.nclasses,
-                         self.n_samples, self.random_state), fout)
-
-    def load(self, load_path):
-        """
-        Load a previously stored PCG class.
-
-        Parameters
-        ----------
-        load_path: str
-            Location on disk to load parsed PCG data.
-
-        Returns
-        -------
-        None
-        """
-        self.X = np.load(os.path.join(load_path, "X.npy"))
-        self.y = np.load(os.path.join(load_path, "y.npy"))
-        with open(os.path.join(load_path, "meta.pkl"), "rb") as fin:
-            (self.basepath, self.class_name_to_id, self.nclasses,
-             self.n_samples, self.random_state) = pickle.load(fin)
-        self.__split_train_test()
-
-    def __load_wav_file(self, doFFT=True):
-        """
-        Loads physio 2016 challenge dataset from self.basepath by crawling the path.
-        For each discovered wav file:
-
-        * Attempt to parse the header file for class label.
-        * Attempt to load the wav file.
-        * Calculate features from the wav file. If doFFT, features are
-          the Fourier transform of the original signal. Else, features are
-          the raw signal itself truncated to a fixed length.
-
-        Parameters
-        ----------
-        doFFT: bool
-            True if features to be calculated are the FFT of the original signal.
-
-        Returns
-        -------
-        None
-        """
-        # First pass to calculate number of samples
-        wav_file_names = []
-        class_labels = []
-        for root, dirs, files in os.walk(self.basepath):
-            # Ignore validation for now!
-            if "validation" in root:
-                continue
-            for file in files:
-                if file.endswith('.wav'):
-                    try:
-                        base_file_name = file.rstrip(".wav")
-                        label_file_name = os.path.join(root, base_file_name + ".hea")
-
-                        class_label = self.__parse_class_label(label_file_name)
-                        class_labels.append(self.class_name_to_id[class_label])
-                        wav_file_names.append(os.path.join(root, file))
-
-                        self.n_samples += 1
-                    except InvalidHeaderFileException as e:
-                        print(e)
-
-        if doFFT:
-            fft_embedding_size = 400
-            highpass_embedding_size = 200
-            X = np.zeros([self.n_samples, fft_embedding_size + highpass_embedding_size])
-        else:
-            # Truncating the length of each wav file to the
-            # min file size (10611) (Note: this is bad
-            # And causes loss of information!)
-            embedding_size = 10611
-            X = np.zeros([self.n_samples, embedding_size])
-
-        for idx, wavfname in enumerate(wav_file_names):
-            rate, wf = wavfile.read(wavfname)
-            wf = normalize(wf.reshape(1, -1), axis=1)  # Normalize along the time axis
-
-            if doFFT:
-                # We only care about the magnitude of each frequency
-                wf_fft = np.abs(fft(wf))
-                wf_fft = wf_fft[:, :fft_embedding_size].reshape(-1)
-
-                # Filter out high frequencies via Butterworth filter
-                nyquist = 0.5 * rate
-                cutoff_freq = 4.0  # Hz
-                b, a = butter(5, cutoff_freq / nyquist, btype='low', analog=False)
-                wf_low_pass = lfilter(b, a, wf)
-
-                # FFT the filtered signal
-                wf_low_pass_fft = np.abs(fft(wf_low_pass))
-                wf_low_pass_fft = wf_low_pass_fft[:, :highpass_embedding_size].reshape(-1)
-
-                features = np.concatenate((wf_fft, wf_low_pass_fft))
-            else:
-                features = wf[:, :embedding_size].reshape(-1)
-
-            X[idx, :] = features
-
-        self.X = X
-        self.y = np.eye(self.nclasses)[class_labels]
-
-    def __parse_class_label(self, label_file_name):
-        """
-        Parses physio bank header files, where the class label
-        is located in the last line of the file.
-
-        Parameters
-        ----------
-        label_file_name: str
-            Path to a specific header file.
-
-        Returns
-        -------
-        class_label: str
-            One of `normal` or `abnormal`.
-        """
-        with open(label_file_name, 'r') as fin:
-            header = fin.readlines()
-
-        comments = [line for line in header if line.startswith("#")]
-        if not len(comments) == 1:
-            raise InvalidHeaderFileException("Invalid label file %s" % label_file_name)
-
-        class_label = str(comments[0]).lstrip("#").rstrip("\r").strip().lower()
-
-        if class_label not in self.class_name_to_id.keys():
-            raise InvalidHeaderFileException("Invalid class label %s" % class_label)
-
-        return class_label
-
-    def __split_train_test(self):
-        """
-        Splits internal features (self.X) and class labels (self.y) into
-        balanced training and test sets using sklearn's helper function.
-
-        Returns
-        -------
-        None
-        """
-        mlData = namedtuple('ml_data', 'X y')
-
-        num_pos, num_neg = np.sum(self.y, axis=0)
-
-        # Remove samples to rebalance classes
-        undersample_rate = num_neg / num_pos
-        over_represented_idxs = self.y[:, 1] == 0
-        under_represented_idxs = self.y[:, 1] == 1
-        random_indexes_to_remove = np.random.rand(self.y.shape[0]) < undersample_rate
-        sample_idxs = (over_represented_idxs & random_indexes_to_remove) | under_represented_idxs
-
-        X_balanced = self.X[sample_idxs, :]
-        y_balanced = self.y[sample_idxs, :]
-
-        X_train, X_test, y_train, y_test = train_test_split(X_balanced, y_balanced, test_size=0.25,
-                                                            random_state=self.random_state)
-
-        self.train = mlData(X=X_train, y=y_train)
-        self.test = mlData(X=X_test, y=y_test)
-
-    def get_mini_batch(self, batch_size):
-        """
-        Helper function for sampling mini-batches from the training set.
-
-        Parameters
-        ----------
-        batch_size: int
-            Number of elements to return in the mini batch.
-
-        Returns
-        -------
-        X: np.ndarray
-            A feature matrix subsampled from self.train.
-
-        y: np.ndarray
-            A one-hot matrix of class labels subsampled from self.train.
-        """
-        random_state = check_random_state(None)  # self.random_state)
-        n_training_samples = self.train.X.shape[0]
-        minibatch_indices = random_state.randint(0, n_training_samples, batch_size)
-
-        return self.train.X[minibatch_indices, :], self.train.y[minibatch_indices, :]
+    # Train the model
+    load_and_train_model(data_path, load_pretrained)
 
 
-class InvalidHeaderFileException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
+
